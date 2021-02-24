f1a872e2ddf82b45b9ad9f25566914055b282e88
Altin Thaqi <ltin.thaqi.520@gmail.com>
2021-02-24 18:59:40 +0100

    Updates

---
+++ precache-manifest.613f7c9b26921ed33777f452a939814f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c12cac25025e4d59c77f96c9b36175",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "f5a8e2095ea674eb0fa6",
+    "url": "/react-deploy/static/css/2.9229e6f1.chunk.css"
+  },
+  {
+    "revision": "06bac6154c1d6b5fabbe",
+    "url": "/react-deploy/static/css/main.37a0d8de.chunk.css"
+  },
+  {
+    "revision": "f5a8e2095ea674eb0fa6",
+    "url": "/react-deploy/static/js/2.3cee59bb.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-deploy/static/js/2.3cee59bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06bac6154c1d6b5fabbe",
+    "url": "/react-deploy/static/js/main.1b07c2e5.chunk.js"
+  },
+  {
+    "revision": "8b55d75d18538817d3c8",
+    "url": "/react-deploy/static/js/runtime-main.f42bf058.js"
+  },
+  {
+    "revision": "63567cd6235a874588c6ee3b2ce74eb2",
+    "url": "/react-deploy/static/media/airbnblogo.63567cd6.png"
+  },
+  {
+    "revision": "19e6d8571defbf2b1537413c0d04d653",
+    "url": "/react-deploy/static/media/bannerimg.19e6d857.jpg"
+  },
+  {
+    "revision": "7eee5fc73d230eec2862ced947e587cf",
+    "url": "/react-deploy/static/media/card1.7eee5fc7.jpg"
+  },
+  {
+    "revision": "b168abc28b214d63720d78528d774bbc",
+    "url": "/react-deploy/static/media/card2.b168abc2.jpg"
+  },
+  {
+    "revision": "e61d25be953c4dc8e4defa29ec10ab6e",
+    "url": "/react-deploy/static/media/card3.e61d25be.jpg"
+  },
+  {
+    "revision": "369626673e76be53202f0b284662f7b7",
+    "url": "/react-deploy/static/media/card4.36962667.jpg"
+  },
+  {
+    "revision": "5a3f7a42ae7d171cb958c9edfcfa6cd8",
+    "url": "/react-deploy/static/media/card5.5a3f7a42.jpg"
+  },
+  {
+    "revision": "a93f241d6df81cb095179e23c2f67972",
+    "url": "/react-deploy/static/media/card6.a93f241d.jpg"
+  }
+]);
